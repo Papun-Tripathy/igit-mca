e48f3bc1099fe691f25af24c64934ad77d7090f5
mrmjpatra <mrmjpatra@gmail.com>
2022-11-16 02:11:23 +0530

    adding help page

--- src/pages/Help/index.jsx
+++ src/pages/Help/index.jsx
@@ -1,29 +1,22 @@
 import React from 'react'
 import './index.css'
 import VisibilityIcon from '@mui/icons-material/Visibility';
+import PlayVideo from './PlayVideo';
+import { Link } from 'react-router-dom';
+
 function index() {
     return (
         <>
             <div className='helpcontent__container'>
-                <div className="helpcontent">
-                    <h3 className='helpcontent__serial'>1</h3>
-                    <h3 className='helpcontent__title'>Install GCC</h3>
-                    <h3 className='helpcontent__desc'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, magnam placeat? Eligendi, fugiat incidunt iste velit ratione inventore numquam excepturi, maiores commodi vel quidem quos illo cum, magni reiciendis eaque!</h3>
-                    <h3 className='helpcontent__btn'><VisibilityIcon/></h3>
-                </div>
-                <div className="helpcontent">
-                    <h3 className='helpcontent__serial'>1</h3>
-                    <h3 className='helpcontent__title'>Install GCC</h3>
-                    <h3 className='helpcontent__desc'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, magnam placeat? Eligendi, fugiat incidunt iste velit ratione inventore numquam excepturi, maiores commodi vel quidem quos illo cum, magni reiciendis eaque!</h3>
-                    <h3 className='helpcontent__btn'><VisibilityIcon/></h3>
-                </div>
-                <div className="helpcontent">
-                    <h3 className='helpcontent__serial'>1</h3>
-                    <h3 className='helpcontent__title'>Install GCC</h3>
-                    <h3 className='helpcontent__desc'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, magnam placeat? Eligendi, fugiat incidunt iste velit ratione inventore numquam excepturi, maiores commodi vel quidem quos illo cum, magni reiciendis eaque!</h3>
-                    <h3 className='helpcontent__btn'><VisibilityIcon/></h3>
-                </div>
-              
+                <Link to={'/playvideo'}>
+                    <div className="helpcontent">
+                        <h3 className='helpcontent__serial'>1</h3>
+                        <h3 className='helpcontent__title'>Install GCC</h3>
+                        <h3 className='helpcontent__desc'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, magnam placeat? Eligendi, fugiat incidunt iste velit ratione inventore numquam excepturi, maiores commodi vel quidem quos illo cum, magni reiciendis eaque!</h3>
+                        <h3 className='helpcontent__btn'><VisibilityIcon /></h3>
+                    </div>
+                </Link>
+
             </div>
         </>
     )
